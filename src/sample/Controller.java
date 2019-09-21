6adcd2be5cf3fecb5f4c6c291c79b1d7383a5a81
cjnicoletti3183 <cjn5307@gmail.com>
2019-09-20 22:31:49 -0400

    Database created. Database connected to code. Data added via DB console.

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,5 +1,47 @@
 package sample;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.ListView;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+import javafx.scene.input.MouseEvent;
+
 public class Controller {
+
+  @FXML
+  private TextField tf_productName;
+
+  @FXML
+  private TextField tf_manufacturer;
+
+  @FXML
+  private TextField tf_itemType;
+
+  @FXML
+  private Button btn_addProduct;
+
+  @FXML
+  private ListView<?> lv_chooseProduct;
+
+  @FXML
+  private ComboBox<?> cb_chooseQuantity;
+
+  @FXML
+  private Button btn_recordProduction;
+
+  @FXML
+  private TextArea ta_productionLog;
+
+  @FXML
+  void addProduct(MouseEvent event) {
+    System.out.println("\"Add Product\" Button has been clicked.");
+  }
+
+  @FXML
+  void recordProduction(MouseEvent event) {
+    System.out.println("\"Record Production\" Button has been clicked.");
+  }
 
 }
--- src/sample/Main.java
+++ src/sample/Main.java
@@ -5,6 +5,11 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 
 /**
  * @author Colby Nicoletti
@@ -31,6 +36,36 @@ public class Main extends Application {
 
 
   public static void main(String[] args) {
+    final String JDBC_DRIVER = "org.h2.Driver";
+    final String DB_URL = "jdbc:h2:./res/ProductDB";
+
+    final String USER = "";
+    final String PASS = "";
+    Connection conn = null;
+    Statement stmt = null;
+
+    try {
+      Class.forName(JDBC_DRIVER);
+      conn = DriverManager.getConnection(DB_URL, USER, PASS);
+      stmt = conn.createStatement();
+
+      //type the sql stuff here
+
+      ResultSet rs = stmt.executeQuery(sql);
+      while (rs.next()) {
+        System.out.println(rs.getString(1));
+      }
+
+      // STEP 4: Clean-up environment
+      stmt.close();
+      conn.close();
+    } catch (ClassNotFoundException e) {
+      e.printStackTrace();
+
+    } catch (SQLException e) {
+      e.printStackTrace();
+    }
+
     launch(args);
   }
 }
