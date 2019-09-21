359baed94ada3b06d2a3ae33ec59ce2803b46e90
cjnicoletti3183 <cjn5307@gmail.com>
2019-09-20 20:55:49 -0400

    Added Product Line, Produce and Production Log tabs.

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -14,22 +14,23 @@ import javafx.stage.Stage;
 
 public class Main extends Application {
 
-    /**
-     * The starting point of the program.
-      * @param primaryStage the first thing a user sees.
-     * @throws Exception
-     */
+  /**
+   * The starting point of the program.
+   *
+   * @param primaryStage the first thing a user sees.
+   * @throws Exception
+   */
 
-    @Override
-    public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
-        primaryStage.show();
-    }
+  @Override
+  public void start(Stage primaryStage) throws Exception {
+    Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+    primaryStage.setTitle("Hello World");
+    primaryStage.setScene(new Scene(root, 300, 275));
+    primaryStage.show();
+  }
 
 
-    public static void main(String[] args) {
-        launch(args);
-    }
+  public static void main(String[] args) {
+    launch(args);
+  }
 }
